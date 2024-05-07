e52d1e4c98c952626fe9c62e5129f6c41b570121
Felipe Santos <felipe52855616@aluno.ms.senac.br>
2024-05-07 11:14:35 -0400

    Exercícios realizados dia 07-05-2024

---
+++ Aula_05_Python-Lista/exercise_41.py
@@ -0,0 +1,8 @@
+print("""
+Enunciado do exercício:
+    41. Calcule as raízes da equação de 2o grau.
+      A variável a tem que ser diferente de zero. Caso seja igual, imprima a mensagem “Não é equação de segundo grau”.
+        i. Se ∆ < 0, não existe real. Imprima a mensagem: Não existe raiz.
+        ii. Se ∆ = 0, existe uma raiz real. Imprima a raiz e a mensagem: Raiz única.
+        iii. Se ∆ ≥ 0, imprima as duas raízes reais.
+""")
