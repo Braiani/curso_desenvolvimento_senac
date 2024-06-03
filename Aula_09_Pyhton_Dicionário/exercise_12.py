e1351ed044d7a8b3c8c2e5e52683f789d0afb0c7
Felipe Braiani <felipe.braianitech@gmail.com>
2024-06-03 07:39:25 -0400

    Atividade Desenvolver Algoritmo

---
+++ Aula_09_Pyhton_Dicionário/exercise_12.py
@@ -0,0 +1,5 @@
+print("""
+Enunciado do exercício:
+    12. Dicionário de Produtos:
+        ○ Crie um programa que simule um carrinho de compras. O usuário pode adicionar produtos (com nome e preço) ao carrinho e, no final, exiba o total da compra.
+""")
