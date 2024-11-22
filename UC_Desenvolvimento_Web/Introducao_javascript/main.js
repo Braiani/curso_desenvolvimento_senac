da3cf59be505e52a70b46854973fb27242fbd08d
Felipe Braiani <felipe.braianitech@gmail.com>
2024-11-22 11:07:52 -0400

    Atividades de evento

--- UC_Desenvolvimento_Web/Introducao_javascript/main.js
+++ UC_Desenvolvimento_Web/Introducao_javascript/main.js
@@ -5,15 +5,44 @@ function quotes(data){
 }
 
 
-console.log(10>20 ? 'Sim': 'Não')
-document.write("<p>Hello World!!!</p>")
-document.write(Date())
+// console.log(10>20 ? 'Sim': 'Não')
+// document.write("<p>Hello World!!!</p>")
+// document.write(Date())
 
-fetch('https://dummyjson.com/quotes?limit=3&skip=10', {
-    method: 'GET',
-    headers: {
-        'Content-Type': 'application/json'
-    }
-})
-.then(res => res.json())
-.then(quotes)
+// fetch('https://dummyjson.com/quotes?limit=3&skip=10', {
+//     method: 'GET',
+//     headers: {
+//         'Content-Type': 'application/json'
+//     }
+// })
+// .then(res => res.json())
+// .then(quotes)
+
+window.onload = function(){
+    let p = document.getElementsByTagName('p')[1]
+    p.style.background = 'blue'
+    p.style.fontSize = '32px'
+
+    p.innerHTML = 'É isso aí mesmo!'
+}
+
+function respPedro(){
+    let li = document.getElementById('pedro')
+    li.innerText = 'Certa Resposta!'
+    li.style.color = 'white'
+    li.style.backgroundColor = 'green'
+}
+
+function respJoao(){
+    let li = document.getElementById('joao')
+    li.innerText = 'Certa Errada!'
+    li.style.color = 'white'
+    li.style.backgroundColor = 'red'
+}
+
+function respMaria(){
+    let li = document.getElementById('maria')
+    li.innerText = 'Certa Errada!'
+    li.style.color = 'white'
+    li.style.backgroundColor = 'red'
+}
