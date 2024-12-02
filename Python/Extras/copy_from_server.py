aef081c1a4463f9eb7f92d6be5eee70313200cca
Felipe Braiani <felipe.braianitech@gmail.com>
2024-12-02 10:38:41 -0400

    Atualizado script

--- Python/Extras/copy_from_server.py
+++ Python/Extras/copy_from_server.py
@@ -4,6 +4,25 @@ import subprocess
 def get_base_path():
     return os.path.dirname(os.path.abspath(sys.argv[0]))
 
+def rodar_git():
+    mensagem = input("Digite a mensagem do commit: ")
+
+    try:
+        git_add = 'git add .'
+        git_commit = f'git commit -m {mensagem}'
+        git_push = 'git push'
+
+        subprocess.run(git_add, shell=True, check=True)
+        subprocess.run(git_commit, shell=True, check=True)
+        
+        realizar_push = input("Deseja realizar o push? ")
+        if realizar_push == 's' or realizar_push == 'sim':
+            subprocess.run(git_push, shell=True, check=True)
+
+    except subprocess.CalledProcessError as e:
+        print(f"Ocorreu um erro ao tentar copiar ou atualizar a pasta: {e}")
+    except Exception as e:
+        print(f"Erro inesperado: {e}")
 def copiar_pasta(pasta_remota = '', servidor = '', diretorio_local = ''):
     if not servidor:
         servidor = input("Digite o endereço do servidor (ex: usuario@192.168.0.1): ")
@@ -18,6 +37,11 @@ def copiar_pasta(pasta_remota = '', servidor = '', diretorio_local = ''):
 
     try:
         subprocess.run(comando, shell=True, check=True)
+
+        realizar_git = input("Deseja realizar o git commit e git push? ").lower()
+        if realizar_git == 's' or realizar_git == 'sim':
+            rodar_git()
+
         print(f"Pasta copiada e atualizada com sucesso para o diretório: {diretorio_local}")
     except subprocess.CalledProcessError as e:
         print(f"Ocorreu um erro ao tentar copiar ou atualizar a pasta: {e}")
