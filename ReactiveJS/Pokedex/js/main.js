4d3462da84a1a9843971a96a66539ca776bc0b73
Felipe Braiani <felipe.santos@ifms.edu.br>
2025-01-28 18:08:48 -0400

    Adicionado API para consumo das informações do Pokemon e iniciado a estilização da página

--- ReactiveJS/Pokedex/js/main.js
+++ ReactiveJS/Pokedex/js/main.js
@@ -1,22 +1,48 @@
 window.onload = getPokemons()
 
-async function getPokemonApi() {
-    const url = 'https://pokeapi.co/api/v2/pokemon'
-
-    try {
-        return await fetch(url).then(response => response.json());
-    } catch (e) {
-        return 'caught';
-    }
-    let 
-        /* .then(response => response.json())
-        .then(data => pokemons = data.body); */
-
-    return pokemons
+async function fetchPokemonApi(url, callback) {
+    await fetch(url).then(response => response.json()).then(callback);
 }
 
 function getPokemons (){
-    let listaPokemons = getPokemonApi()
+    const url = 'https://pokeapi.co/api/v2/'
+    const lista = document.getElementById('lista');
     
-    console.log(listaPokemons)
+    fetchPokemonApi(url + 'pokemon?limit=40&offset=0', (data) => {
+        
+        let pokemons = data.results;
+
+        pokemons.forEach(pokemon => {
+
+            fetchPokemonApi(pokemon.url, (options) => {
+                //console.log(options.stats);
+
+                let listaAbilidades = `
+                <ul class="list-group list-group-flush text-capitalize">
+                `;
+                
+                options.stats.map(abilidade => {
+                    listaAbilidades += `<li class="list-group-item m-0">${abilidade.stat.name}: ${abilidade.base_stat}</li>`;
+                });
+
+                listaAbilidades += `</ul>`;
+
+
+                html = `<div class="col-3 my-2">
+                            <div class="card">
+                                <img src="${options.sprites.other.home.front_default}" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <h5 class="card-title text-capitalize">${pokemon.name}</h5>
+                                    <p class="card-text">
+                                        <strong>Estatísticas:</strong>
+                                        ${listaAbilidades}
+                                    </p>
+                                    <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+                                </div>
+                            </div>
+                        </div>`;
+                lista.innerHTML += html;
+            });
+        });
+    });
 }
